af7ec9da757a450e1b3abb8c161690e80cbd8ace
Chatzis Nick <frspm@tuta.io>
2020-05-31 10:10:30 +0100

    separated coc install

--- get-nvim.sh
+++ get-nvim.sh
@@ -6,7 +6,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
 cd /tmp && mkdir neovim-temp && cd neovim-temp &&
 git clone https://github.com/chn-cynic/neovim.git &&
-cp -r /tmp/neovim-temp/neovim/* ~/.config/nvim &&
-rm -rf /tmp/neovim-temp/ &&
-nvim +PlugInstall +qall &&
-nvim -c "CocInstall coc-lists"
+cp -r /tmp/neovim-temp/neovim/* ~/.config/nvim/ &&
+#rm -rf /tmp/neovim-temp/ &&
+nvim +PlugInstall +qall
