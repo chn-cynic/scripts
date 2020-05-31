a48b7d61ebeeb8414e76354f40b4f279a5261bb3
Chatzis Nick <frspm@tuta.io>
2020-05-31 09:19:36 +0100

    added a force flag upon tmp deletion

--- get-nvim.sh
+++ get-nvim.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
+rm -rf ~/.config/nvim &&
 mkdir -p "${XDG_DATA_HOME:-$HOME/.config/nvim}" &&
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
-cd $HOME && mkdir neovim-temp && cd neovim-temp &&
+cd /tmp && mkdir neovim-temp && cd neovim-temp &&
 git clone https://github.com/chn-cynic/neovim.git &&
-cp -r ~/neovim-temp/neovim/* ~/.config/nvim &&
+cp -r /tmp/neovim-temp/neovim/* ~/.config/nvim &&
+rm -rf /tmp/neovim-temp/ &&
 nvim +PlugInstall +qall &&
-nvim -c "CocInstall coc-tsserver"
+nvim -c "CocInstall coc-lists"
